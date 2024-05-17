2dd2cdc8782c533c6d8aff85329bbaa2b3b2bdf7
rush1998 <rushabh.m.patel1998@gmail.com>
2024-05-17 15:33:15 -0400

    added new js functionality

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ function showSidebar(){
     sidebar.style.display = 'flex'
 }
 
+function hideLines(){
+    const nav5 = document.querySelector('.nav5')
+    nav5.style.display = 'none'
+}   
+
 function hideSidebar(){
-    const sidebar = document.querySelector('sidebar')
+    const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+// function hideLines(){
+//     const nav5 = document.querySelector('.nav5')
+//     nav5.style.display = 'flex'
+// }   
